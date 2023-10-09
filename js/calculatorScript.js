e3d1c827f3192c61b14f133003c0c289c190630d
benjaAguilar <benjaaguilar02@gmail.com>
2023-10-09 10:25:52 -0300

    added createKeyboard() and showOnDisplay()
    now we assigned all the number values with a for each on
    createKeyboard() who pass an addEventListener with the function
    showOnDisplay()

--- js/calculatorScript.js
+++ js/calculatorScript.js
@@ -1,9 +1,32 @@
 let firstNum;
 let operator;
 let secondNum;
+let btnNumbers = document.querySelectorAll('.btns');
+
+function showOnDisplay(element){
+    console.log(element);
+}
+
+function createKeyboard(){
+    let indexBtn = 1;
+
+    btnNumbers.forEach(button => {
+        let i = indexBtn;
+        if(indexBtn === 10){
+            indexBtn = 0;
+            i = 0;
+
+        } 
+        button.addEventListener("click", () => {showOnDisplay(i);});
+        indexBtn++;
+        console.log(indexBtn);
+    });
+}
+
+createKeyboard();
 
 let getResult = function(string) {
     return (new Function( 'return (' + string + ')' )());
 }
 
-console.log(getResult("1+5"))
+
